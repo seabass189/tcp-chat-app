56b866840cd7395d6a27c264d73a8b80ce2c3b4d
Sebastian Hernandez <33647914+seabass189@users.noreply.github.com>
2020-05-01 14:22:03 -0700

    Added comments to Server

--- src/Server.java
+++ src/Server.java
@@ -4,10 +4,10 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 /**
- * 
- */
-
-/**
+ * This class represents a Server. This class primarily controls the UserHandlers
+ * as well as dealing with any new connections. New connections will be given a 
+ * new UserHandler and User
+ *
  * @author sebas
  * 
  */
@@ -21,18 +21,29 @@ public class Server {
 	 */
 	private static final long serialVersionUID = 0L;
 
+	/**
+	 * The port where Server will listen for new connections is set before execution
+	 */
 	private static int listeningPort = 6789;
 
 	/**
-	 * 
+	 * This field is the ServerSocket which will listen for new connections.
+	 * It will be instantiated using the listeningPort
 	 */
 	private static ServerSocket welcomeSocket;
 
 	/**
-	 * 
+	 * This field is an ArrayList that contains all of the current UserHandlers
 	 */
 	private static ArrayList<UserHandler> currentUserHandlers = new ArrayList<UserHandler>();
 	
+	/**
+	 * This function will take received connection request messages and the socket it was sent on,
+	 * and assign the connection with a User, which will be passed as an argument into a new
+	 * UserHandler. The new UserHandler is then added to the currentUserHandlers arraylist
+	 * @param message
+	 * @param cSocket
+	 */
 	private static void addNewClient(Message message, Socket cSocket) {
 		User newUser = new User(message.getMessageText());
 		UserHandler newUH;
@@ -46,6 +57,7 @@ public class Server {
 	}
 
 	/**
+	 * The main method will listen for any incoming messages/connections on the ServerSocket
 	 * @param args
 	 */
 	public static void main(String[] args) {
